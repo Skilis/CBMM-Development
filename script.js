b1fb0670cf6a80414cd102b8bbc340ba1a6abb81
Skilis <53493974+Skilis@users.noreply.github.com>
2020-10-12 13:25:39 -0700

    updated script js
    took space out

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ $("#navbar a").on('click', function(event) {
     event.preventDefault();
     var hash = this.hash;
     $('html, body').animate({
-      scrollTop: $(hash).offset() .top
+      scrollTop: $(hash).offset().top
     }, 800, function(){
       window.location.hash = hash;
     });
